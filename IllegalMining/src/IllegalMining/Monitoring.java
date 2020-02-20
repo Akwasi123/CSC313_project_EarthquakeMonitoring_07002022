6c975d312e35e9f4d8cc9824e8bf8a23bd104b65
Akwasi Appiadjei Asante-Krobea <49639685+Akwasi123@users.noreply.github.com>
2020-02-20 23:29:41 +0000

    Commit

---
+++ IllegalMining/src/IllegalMining/Monitoring.java
@@ -0,0 +1,109 @@
+/** 
+ * @author Kobina Asempa Takyi
+ * @author Akwasi Appiadjei Asante - Krobea
+ * @author Leslie Nartey - Kodjoe
+ * 
+ * @version 1.0.0
+ * 
+*/
+
+package IllegalMining;
+import java.io.FileReader;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.ArrayList;
+
+// This creates an array which references data from the observatory class.
+
+public class Monitoring{
+    public static List<Observatory> monitor = new ArrayList<>();
+
+
+    public Monitoring(){
+
+    }
+
+    // This method is used to print all events in a particular observatory.
+
+    public void printAllEvents(){
+
+        for(Observatory event:monitor){
+            System.out.println(event.printInfo());
+            System.out.println("\n");
+
+        }
+
+    }
+
+    /**
+     * This method is used to retrieve the observatory with the largest average color value.
+     * @return obsName
+     */
+
+    public String getObservatoryWithLargestAverageColorValue() {
+        double val = 0.0;
+        String obsName = "";
+        for(Observatory event:monitor){
+            if(event.getAverageColorValue()>val){
+                val = event.getAverageColorValue();
+                obsName = event.getObservatoryName();
+
+            }
+        }
+        return obsName;
+
+    }
+
+
+    /**
+     * This method returns the galamsey with the largest average value.
+     * @return value
+     */
+
+     public double getLargestAverage(){
+         double value = 0.0;
+         for(Observatory event:monitor){
+             if(event.getAverageColorValue()>value){
+                 value = event.getAverageColorValue();
+
+             }
+         }
+         return value;
+     }
+
+
+    /**
+     * This method returns the galamsey with the largest color value.
+     * @return value
+     */
+
+    public int getLargestValueRecorded(){
+        int value = 0;
+        for(Observatory event:monitor){
+            if(event.getLargestColorValue() > value){
+                value = event.getLargestColorValue();
+            }
+        }
+        return value;
+    }
+
+    
+    /**
+     * This method returns all galamsey recorded with color value greater than the given number.
+     * @param value
+     * @return allGalamEvents
+     */
+
+    public List<Galamsey> getObservatoryRecordsGreaterThan(int value){
+        List<Galamsey> allGalamEvents = new ArrayList<>();
+        for(Observatory event:monitor){
+            for(Galamsey gEvent:event.getGalamseyEventsGreaterThan(value)){
+                allGalamEvents.add(gEvent);
+            }
+
+        }
+        return allGalamEvents;
+
+    }
+
+}
---
+++ IllegalMining/src/IllegalMining/ObservatoryController.java
@@ -0,0 +1,89 @@
+package IllegalMining;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+import java.sql.*;
+
+
+public class ObservatoryController {
+
+    Statement stmt;
+
+    private Connection conn;
+
+    @FXML
+    private Button store2;
+
+
+    @FXML
+    private Button back2;
+
+
+    @FXML
+    public TextField obsName;
+
+    @FXML
+    public TextField country;
+
+    @FXML
+    public TextField yearStarted;
+
+    @FXML
+    public TextField area;
+
+
+    public void getObsName(){
+        obsName.getText();
+    }
+
+    public void getCountry(){
+        country.getText();
+    }
+
+    public void getyearStarted(){
+        yearStarted.getText();
+    }
+
+    public void getArea(){
+        area.getText();
+    }
+
+    public void setObsStore() {
+
+        Mining database = new Mining();
+        conn = database.getDbConnection();
+
+        String query = "INSERT INTO Observatory(ObsName, country, year_started, area_covered)" +
+                "VALUES ('"+obsName.getText()+"','"+country.getText()+"','"+yearStarted.getText()+"','"+area.getText()+"')";
+
+        try {
+            stmt = conn.createStatement();
+            stmt.executeUpdate(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void setObsBack(ActionEvent event) throws IOException {
+        Parent viewParent = FXMLLoader.load(getClass().getResource("MenuScene.fxml"));
+        Scene viewScene = new Scene(viewParent);
+
+        //Get stage(Window) Information
+        Stage window = (Stage)((Node) event.getSource()).getScene().getWindow();
+
+
+        window.setScene(viewScene);
+        window.show();
+    }
+}
